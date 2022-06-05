7cb1ceea45a4346133450d41b2c3badd44b59e0b
NadirSmith <105950655+NadirSmith@users.noreply.github.com>
2022-06-05 12:24:54 -0600

    Agregar archivos de proyecto.

---
+++ PaginaWeb1/Controllers/Contactoscontroller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaginaWeb1.Models;
+namespace PaginaWeb1.Controllers
+{
+    public class Contactoscontroller : Controller
+    {
+        List<Datoscontacto> datos = new List<Datoscontacto>() {
+       new Datoscontacto() { Correo="hbdj@mfdq.com",Nombre="nadir"},
+        };
+        public IActionResult Index()
+        {
+            return View(datos);
+        }
+    }
+}
---
+++ PaginaWeb1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaginaWeb1.Models;
+using System.Diagnostics;
+
+namespace PaginaWeb1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contactanos()
+        {
+          Datoscontacto dc = new Datoscontacto();
+           // dc.Nombre = "nadir";
+           // dc.Correo = "nadir@bensa.cr.com";
+           // dc.Comentario = "esta lindo";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contactanos(Datoscontacto DC)
+        {
+            DC.Comentario = "Comentario genérico";
+            return View ("ContactenosGracias",DC); 
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
